acd99061e490cfbd4b429d9f8d5e4b2fd99f0fa3
M <M@DESKTOP>
2024-09-09 18:16:36 +0300

    Added PhotoStock API

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "photo_stock_catalog";
+    options.RequireHttpsMetadata = false;
+}); //Added by MB
+
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+}); // Added by MB, bütün kontrollerin ustune ekleme yapmak
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseAuthentication(); //MB
+
+app.MapControllers();
+
+app.Run();
